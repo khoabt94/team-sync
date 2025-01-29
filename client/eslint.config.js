0e57f327e86cf693e6a02d88af2183bff1ed034e
spartan-khoatienbui <khoa.bui@c0x12c.com>
2025-01-29 17:10:05 +0700

    feat: add eslint to client

--- client/eslint.config.js
+++ client/eslint.config.js
@@ -1,28 +1,110 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
+import jslint from "@eslint/js";
+import globals from "globals";
+import reactHooks from "eslint-plugin-react-hooks";
+import reactRefresh from "eslint-plugin-react-refresh";
+import tslint from "typescript-eslint";
+import perfectionist from "eslint-plugin-perfectionist";
+import react from "eslint-plugin-react";
+import tailwind from "eslint-plugin-tailwindcss";
+import storybook from "eslint-plugin-storybook";
+import prettier from "eslint-plugin-prettier";
 
-export default tseslint.config(
-  { ignores: ['dist'] },
+export default tslint.config(
+  { ignores: ["dist", "src/router.ts", "!.storybook", "storybook-static"] },
   {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
-    files: ['**/*.{ts,tsx}'],
+    extends: [jslint.configs.recommended, ...tslint.configs.recommended, ...tslint.configs.stylistic],
+    files: ["**/*.{ts,tsx}"],
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
     },
     plugins: {
-      'react-hooks': reactHooks,
-      'react-refresh': reactRefresh,
+      "react-hooks": reactHooks,
+      react,
+      perfectionist,
     },
     rules: {
       ...reactHooks.configs.recommended.rules,
-      'react-refresh/only-export-components': [
-        'warn',
-        { allowConstantExport: true },
+      "react/jsx-curly-brace-presence": ["error", { props: "never", children: "ignore", propElementValues: "always" }],
+      "perfectionist/sort-imports": [
+        "error",
+        {
+          type: "alphabetical",
+          internalPattern: [
+            "@/**",
+            "@icons",
+            "@images",
+            "@router",
+            "@shared/**",
+            "@configs/**",
+            "@api/**",
+            "@styles/**",
+          ],
+          groups: [
+            "type",
+            ["builtin", "external"],
+            "internal-type",
+            "internal",
+            "side-effect",
+            ["parent-type", "sibling-type", "index-type"],
+            ["parent", "sibling", "index"],
+            "object",
+            "unknown",
+          ],
+        },
+      ],
+      "perfectionist/sort-exports": ["error", { type: "alphabetical" }],
+      "@typescript-eslint/no-unused-vars": [
+        "error",
+        {
+          args: "all",
+          argsIgnorePattern: "^_",
+          caughtErrors: "all",
+          caughtErrorsIgnorePattern: "^_",
+          destructuredArrayIgnorePattern: "^_",
+          varsIgnorePattern: "^_",
+          ignoreRestSiblings: true,
+        },
+      ],
+      "@typescript-eslint/no-unused-expressions": "error",
+      "@typescript-eslint/consistent-type-definitions": ["error", "type"],
+      "@typescript-eslint/array-type": ["error", { default: "generic" }],
+      "react/function-component-definition": ["error", { namedComponents: "function-declaration" }],
+      "prefer-template": "error",
+      "react/jsx-key": "error",
+      "react/no-array-index-key": "error",
+    },
+    settings: {
+      react: {
+        version: "detect",
+      },
+    },
+  },
+  {
+    files: ["**/*.{js,jsx,ts,tsx}"],
+    rules: {
+      "no-restricted-imports": [
+        "error",
+        {
+          patterns: [
+            { group: ["../*"], message: "Please use absolute import instead relative import." },
+            {
+              group: ["react-router-dom"],
+              importNames: ["Navigate", "Link", "redirect", "useParams", "useNavigate"],
+              message: "Please import from '@/router' instead of 'react-router-dom'.",
+            },
+          ],
+        },
       ],
     },
   },
-)
+  {
+    files: ["**/*.{ts,tsx}"],
+    extends: [...tailwind.configs["flat/recommended"]],
+    plugins: { prettier, "react-refresh": reactRefresh },
+    rules: {
+      "prettier/prettier": ["error"],
+      "react-refresh/only-export-components": ["warn", { allowConstantExport: true }],
+    },
+  },
+);
