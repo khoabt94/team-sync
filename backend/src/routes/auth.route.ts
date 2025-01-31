31a640448685d75b606993877498366a503009d8
spartan-khoatienbui <khoa.bui@c0x12c.com>
2025-01-31 11:52:11 +0700

    fix: route folder

---
+++ backend/src/routes/auth.route.ts
@@ -0,0 +1,29 @@
+import passport from "passport";
+import { config } from "@config/app.config";
+import { Router } from "express";
+import { authControllers } from "@modules/auth";
+
+// '/auth'
+const authRoutes = Router();
+const failureRedirect = `${config.CLIENT_GOOGLE_CALLBACK_URL}?status=failure`;
+
+authRoutes.post("/register", authControllers.registerUserByEmail);
+authRoutes.post("/login", authControllers.emailLogin);
+authRoutes.post("/logout", authControllers.logOutController);
+
+authRoutes.get(
+  "/google",
+  passport.authenticate("google", {
+    scope: ["profile", "email"],
+  }),
+);
+
+authRoutes.get(
+  "/google/callback",
+  passport.authenticate("google", {
+    failureRedirect,
+  }),
+  authControllers.googleLoginCallback,
+);
+
+export { authRoutes };
---
+++ backend/src/routes/index.ts
@@ -0,0 +1,5 @@
+export * from "./auth.route";
+export * from "./project.route";
+export * from "./task.route";
+export * from "./user.route";
+export * from "./workspace.route";
